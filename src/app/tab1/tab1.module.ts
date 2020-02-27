f63602bd0894411ad2865ffc7fc3231a2a800bfd
shaonkhansur <shaonkhansur@gmail.com>
2020-02-27 19:42:52 +0600

    initial implementation

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -2,16 +2,16 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private photoService: PhotoService,
+  ) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.createForm()
 
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      textinput: ['', Validators.required],
+      freetext: ['', Validators.required],
+      request: ['', Validators.required],
+      measurement: ['', Validators.required],
+      photo: ['', Validators.required],
+      date: ['', Validators.required],
+      signature: ['', Validators.required],
+      details: ['', Validators.required],
+    })
+  }
+
+  takePhoto() {
+    console.log('clicked');
+    this.photoService.addNewToGallery()
+  }
+
+  onSubmit() {
+    
+  }
 }
